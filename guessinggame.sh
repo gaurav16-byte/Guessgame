9d514376ab04245ea5c3b5cea0ba1020bac48b58
gaurav16-byte <gauravtaxali900@gmail.com>
2020-06-26 14:51:17 -0400

    Added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+function gues {
+	echo "Guess the number"
+	read guess
+
+}
+
+gues
+
+while [[ $guess -ne 3 ]]
+do
+	if [[ $guess -lt 3 ]]
+	then
+		echo "The number is too low"
+		gues
+	elif [[ $guess -gt 3 ]]
+	then
+		echo "Number is too high"
+		gues
+	else
+		echo "Congratulations.."
+
+fi
+done
+echo "Congratulations.. You have guessed the number"
